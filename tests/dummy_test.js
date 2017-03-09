3a0f4cde72ca445f137dc3b2dadb9a435980a261
Markus Felten <Markus.Felten@gmx.de>
2017-03-09 23:40:00 +0100

    chore(test): mocha -> ava

--- tests/dummy_test.js
+++
@@ -1,8 +0,0 @@
-/* global xdescribe, describe, it, xit, before, beforeEach, after, afterEach */
-/* jslint node: true, esnext: true */
-'use strict';
-
-const chai = require('chai'),
-  assert = chai.assert,
-  expect = chai.expect,
-  should = chai.should();
