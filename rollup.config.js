dc4b1d368d62f5118083df6029eecd59efacd094
Markus Felten <Markus.Felten@gmx.de>
2017-08-20 16:42:37 +0200

    chore(rollup): rollup@0.48.0 - wip

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,10 @@ import babel from 'rollup-plugin-babel';
 import pkg from './package.json';
 
 export default {
-  targets: [{
+  output: {
     dest: pkg.main,
     format: 'cjs'
-  }],
+  },
   plugins: [
     babel({
       babelrc: false,
