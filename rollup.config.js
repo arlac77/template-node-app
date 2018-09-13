1b597d74da242de389bf55dc5fbdecbc4306ee23
Markus Felten <Markus.Felten@gmx.de>
2018-09-13 15:43:28 +0200

    fix(rollup): generate node call with --experimental-modules --experimental-worker

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,8 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import executable from 'rollup-plugin-executable';
-import cleanup from 'rollup-plugin-cleanup';
-import pkg from './package.json';
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import executable from "rollup-plugin-executable";
+import cleanup from "rollup-plugin-cleanup";
+import pkg from "./package.json";
 
 export default [
   ...Object.keys(pkg.bin || {}).map(name => {
@@ -10,8 +10,9 @@ export default [
       input: `src/${name}.js`,
       output: {
         file: pkg.bin[name],
-        format: 'cjs',
-        banner: '#!/usr/bin/env node',
+        format: "cjs",
+        banner:
+          "#!/usr/bin/env node --experimental-modules --experimental-worker",
         interop: false
       },
       plugins: [resolve(), commonjs(), cleanup(), executable()]
@@ -21,7 +22,7 @@ export default [
     input: pkg.module,
     output: {
       file: pkg.main,
-      format: 'cjs',
+      format: "cjs",
       interop: false
     },
     plugins: [resolve(), commonjs(), cleanup()]
