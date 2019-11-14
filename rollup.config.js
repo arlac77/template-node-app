5e086d8e26158578590ad7c1caf36e8b252aa4b5
Markus Felten <Markus.Felten@gmx.de>
2019-11-14 10:27:17 +0100

    chore(rollup) switch to @rollup/plugin-json

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,13 @@
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import executable from "rollup-plugin-executable";
-import json from "rollup-plugin-json";
+import json from "@rollup/plugin-json";
 import cleanup from "rollup-plugin-cleanup";
 import builtins from "builtin-modules";
 import pkg from "./package.json";
 
 const external = [...builtins];
+const extensions = ["js", "mjs", "jsx", "tag"];
 
 export default [
   ...Object.keys(pkg.bin || {}).map(name => {
@@ -29,7 +30,7 @@ export default [
           compact: true
         }),
         cleanup({
-          extensions: ["js", "mjs", "jsx", "tag"]
+          extensions
         }),
         executable()
       ],
@@ -48,7 +49,7 @@ export default [
       commonjs(),
       resolve(),
       cleanup({
-        extensions: ["js", "mjs", "jsx", "tag"]
+        extensions
       })
     ]
   }
