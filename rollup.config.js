e2056671c7eee83729e99fec4433feb4dce09665
Markus Felten <Markus.Felten@gmx.de>
2019-11-14 12:43:18 +0100

    fix(rollup): ony generate pkg main if main and module are declared

--- rollup.config.js
+++ rollup.config.js
@@ -8,49 +8,46 @@ import pkg from "./package.json";
 
 const external = [...builtins];
 const extensions = ["js", "mjs", "jsx", "tag"];
-
-export default [
-  ...Object.keys(pkg.bin || {}).map(name => {
-    return {
-      input: `src/${name}-cli.mjs`,
-      output: {
-        file: pkg.bin[name],
-        format: "cjs",
-        banner:
-          '#!/bin/sh\n":" //# comment; exec /usr/bin/env node --experimental-modules --experimental-wasm-modules "$0" "$@"',
-        interop: false,
-        externalLiveBindings: false
-      },
-      plugins: [
-        commonjs(),
-        resolve(),
-        json({
-          include: "package.json",
-          preferConst: true,
-          compact: true
-        }),
-        cleanup({
-          extensions
-        }),
-        executable()
-      ],
-      external
-    };
+const plugins = [
+  commonjs(),
+  resolve(),
+  json({
+    include: "package.json",
+    preferConst: true,
+    compact: true
   }),
-  {
+  cleanup({
+    extensions
+  })
+];
+
+const config = Object.keys(pkg.bin || {}).map(name => {
+  return {
+    input: `src/${name}-cli.mjs`,
+    output: {
+      plugins: [executable()],
+      banner:
+        '#!/bin/sh\n":" //# comment; exec /usr/bin/env node --experimental-modules --experimental-wasm-modules "$0" "$@"',
+      file: pkg.bin[name]
+    }
+  };
+});
+
+if (pkg.module !== undefined && pkg.main !== undefined) {
+  config.push({
     input: pkg.module,
     output: {
-      file: pkg.main,
-      format: "cjs",
-      interop: false,
-      externalLiveBindings: false
-    },
-    plugins: [
-      commonjs(),
-      resolve(),
-      cleanup({
-        extensions
-      })
-    ]
-  }
-];
+      file: pkg.main
+    }
+  });
+}
+
+export default config.map(c => {
+  c.output = {
+    interop: false,
+    externalLiveBindings: false,
+    format: "cjs",
+    ...c.output
+  };
+  return { plugins, external, ...c };
+});
